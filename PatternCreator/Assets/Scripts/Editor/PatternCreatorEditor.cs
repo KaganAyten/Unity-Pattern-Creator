9c41e79e97e94426918cd5e8561585761cede6b7
Kagan Ayten <kaan.aytn@hotmail.com>
2021-08-04 19:04:17 +0300

    First commit with demo

---
+++ PatternCreator/Assets/Scripts/Editor/PatternCreatorEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(PatternCreator))]
+public class PatternCreatorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        PatternCreator myTarget = (PatternCreator)target;
+
+        if(GUILayout.Button("Create Pattern"))
+        {
+            myTarget.CreatePattern();
+        }
+    }
+}
---
+++ PatternCreator/Assets/Scripts/PatternCreator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PatternCreator : MonoBehaviour
+{
+    public GameObject patternObject;
+    public Transform spawnPoint;
+    public Vector3 distance;
+    public float amountOfRepeat;
+    public string prefabName;
+
+    [Header("Creation options")]
+    public bool destroyFromScene;
+    public bool createPrefab;
+    public void CreatePattern()
+    {
+        GameObject tempParent = new GameObject();
+        tempParent.name = prefabName;
+        tempParent.transform.position = spawnPoint.position;
+
+        GameObject[] objectList = new GameObject[(int)amountOfRepeat];
+        for (int i = 0; i < amountOfRepeat; i++)
+        {
+            GameObject temp = Instantiate(patternObject);
+            temp.transform.position = spawnPoint.transform.position + (distance * i);
+            temp.transform.parent = tempParent.transform;
+        }
+        if (createPrefab)
+        {
+            string prefabPath = "Assets/"+ prefabName+".prefab";
+            PrefabUtility.SaveAsPrefabAsset(tempParent, prefabPath);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+        if (destroyFromScene)
+        {
+            DestroyImmediate(tempParent.gameObject);
+        }
+    }
+}
